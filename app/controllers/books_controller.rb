70ced626f2956b4b943e571ad9fdb0c938465f56
lalitmll <palariya.lalit@mahhindralogistics.com>
2025-05-08 00:00:19 +0530

    Added book controller and all actions in that and with view templates

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @books = Book.includes(:user).order(created_at: :desc)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = current_user.books.build
+  end
+
+  def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to @book, notice: "Book recommended successfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :image)
+  end
+end
