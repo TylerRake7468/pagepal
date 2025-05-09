5638753ed3505b8564836e6bcef415ce94c9565b
lalitmll <palariya.lalit@mahhindralogistics.com>
2025-05-09 17:23:11 +0530

    Fix the logout button issue

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import "rails-ujs"; // Ensure this line is present
 import "@hotwired/turbo-rails"
 import 'flowbite'
-
-
-const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
+const application = Application.start();
+application.debug = false;
+window.Stimulus = application;
 export { application }
